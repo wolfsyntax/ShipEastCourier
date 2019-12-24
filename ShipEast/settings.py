2799975eadf641d962bfed586e9fae3e7963f574
Wolf Syntax <wolfsyntax@yahoo.com>
2019-12-25 07:19:23 +0800

    Fix UI missing admin assets Attempt#1

--- ShipEast/settings.py
+++ ShipEast/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*h6%4z_ye=i794f50!t#)9rltqszw=_y(0u)!s4afg6+-y5p)7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['shipeastcouriers.com','realmad.pythonanywhere.com','127.0.0.1', '*']
 
 
@@ -139,6 +139,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'public'),
     os.path.join(BASE_DIR, 'static'),
 )
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
